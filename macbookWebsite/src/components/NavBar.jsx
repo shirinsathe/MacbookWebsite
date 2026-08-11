7b2ca89758c1a51c4b211bf655a2d21bf27431d6
Shirin Sathe <shirinsathe123@gmail.com>
2026-08-11 17:28:31 -0400

    setup + Navigation bar implemented

---
+++ macbookWebsite/src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { navLinks } from '../constants'
+
+const NavBar = () => {
+  return (
+    <header>
+        <nav>
+            <img src="/logo.svg" alt="Apple logo" />
+
+            <ul>
+                {navLinks.map((link) => (
+                    <li key={link.label}>
+                        <a href={link.label}>{link.label}</a>
+                    </li>
+                ))}
+            </ul>
+
+            <div className="flex-center gap-3">
+                <button>
+                    <img src="/search.svg" alt="Search"></img>
+                </button>
+                <button>
+                    <img src="/cart.svg" alt="Cart"></img>
+                </button>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default NavBar

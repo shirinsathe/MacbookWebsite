4970b9aabd29884f3ac67cfe9ac6c83772721e91
Shirin Sathe <shirinsathe123@gmail.com>
2026-08-16 17:50:00 -0400

    added component highlights grid and footer

--- macbookWebsite/src/components/Features.jsx
+++ macbookWebsite/src/components/Features.jsx
@@ -12,7 +12,7 @@ import gsap from 'gsap';
 
 const ModelScroll = () => {
   const groupRef = useRef(null);
-  const isMobile = useMediaQuery({query: '(max-width: 1024px)'})
+  const isMobile = useMediaQuery({query: '(max-width: 1024px)'});
   const { setTexture } = useMacbookStore();
 
   //preload all the vids during component mount
--- macbookWebsite/src/components/Footer.jsx
+++ macbookWebsite/src/components/Footer.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { footerLinks } from '../constants'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className='info'>
+        <p>More ways to shop: Find an Apple Store or other retailer near you. Or call (123) 456-7890.</p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+
+      <hr />
+
+      <div className='links'>
+        <p>Copyright © 2026 Apple Inc. All rights reserved.</p>
+        <ul>
+          {footerLinks.map(({link, label}) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   )
 }
 
